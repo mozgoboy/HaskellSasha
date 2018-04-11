dc853f8f6f3925c7e2ccccfa3d73750d90446b88
Str0bile <37554710+Str0bile@users.noreply.github.com>
2018-04-11 21:25:11 +0300

    Update Disj_S.hs

--- Disj_S.hs
+++ Disj_S.hs
@@ -7,43 +7,44 @@ data Literal = Var Int
 
 type Disj = [Literal]
 
-
-
-
 snot :: Literal -> Literal
 snot (Var x) = (Not x)
 snot (Not x) = (Var x)
 
-headL :: Disj -> Literal
-headL (a:as) = a
+resolution :: Disj -> Disj -> Disj
+resolution as bs = simplify (f as bs []) where
+        f :: Disj -> Disj -> Disj -> Disj
+        f []     _      buff = buff
+        f _      []     buff = buff
+        f [a]    (b:bs) buff | a == snot b  = buff ++ bs
+                             | bs == []     = [b]
+                             | otherwise    = [b] ++ (f [a] bs (buff))
 
-removeHead :: Disj -> Disj
-removeHead [] = []
-removeHead (a:as) = as
-
-f :: Disj -> Disj -> Disj -> Disj
-f [] _ buff = buff
-f _ [] buff = buff
-
-f [a] (b:bs) buff | a == snot b  =  buff ++ bs
-                  | bs == []     =  [b]
-                  | otherwise    =  [b] ++ (f [a] bs (buff))
-
-f (a:as) (b:bs) buff | f [a] (b:bs) buff /= (b:bs)  =  buff ++ as ++ f [a] (b:bs) buff
-                     | as == []                     =  buff ++ (f as (b:bs) buff)
-                     | otherwise                    =  f as (b:bs) (buff ++ [a])
+        f (a:as) (b:bs) buff | f [a] (b:bs) buff /= (b:bs)  = buff ++ as ++ f [a] (b:bs) buff
+                             | as == []                     = buff ++ (f as (b:bs) buff)
+                             | otherwise                    = f as (b:bs) (buff ++ [a])
 
 
-simplify1 :: Disj -> Disj -> Disj
-simplify1 [a] buff = buff
-simplify1 (a:as) buff | a == headL as  =  simplify1 (a : removeHead as) buff
-                      | otherwise      =  simplify1 (a : removeHead as) (buff ++ [headL as])
-
-simplify :: Disj -> Disj -> Disj
-simplify [] buff  = buff
-simplify [a] buff = buff ++ [a]
-simplify (a:as) buff = simplify (simplify1 (a:as) []) (buff ++ [a])
+simplify :: Disj -> Disj
+simplify [] = []
+simplify (x:xs) | x `elem` xs  = simplify xs
+                | otherwise    = x : simplify xs
 
 
---f [Var 1,Var 2,Var 5,Var 6,Not 6] [Var 3, Var 4,Not 5,Var 7,Var 8] []
---simplify simplify [Var 1, Var 3, Var 1, Var 2, Var 2, Var 1, Not 1, Var 1, Var 4, Var 3] []
+proof :: [Disj] -> Bool
+proof as | fst (fullPass as [])        = True
+         | snd (fullPass as []) == []  = False
+         | otherwise                   = proof (as ++ snd (fullPass as []))
+         where
+          partPass :: Disj -> [Disj] -> [Disj] -> (Bool, [Disj])
+          partPass a []     buff = (False, buff)
+          partPass a (b:bs) buff | resolution a b == []  = (True, buff)
+                                 | otherwise             = partPass a bs (resolution a b : buff)
+          fullPass :: [Disj] -> [Disj] -> (Bool, [Disj])
+          fullPass []     buff = (False, buff)
+          fullPass (a:as) buff | fst (partPass a as [])  = (True, buff)
+                               | otherwise               = fullPass as (buff ++ snd (partPass a as []))
+
+
+
+--resolution [Var 1,Var 2,Var 5,Var 6,Not 6] [Var 3, Var 4,Not 5,Var 7,Var 8]
